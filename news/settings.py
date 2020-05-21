beea2e15efc358cfb3b6548583983b3d79711d9f
Mariia Borodulia <maryborodulya@gmail.com>
2020-05-22 01:41:55 +0300

    fix db

--- news/settings.py
+++ news/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = "news.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": os.getenv("DATABASE_URL", "db"),
-        "PORT": 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "postgres",
+    #     "USER": "postgres",
+    #     "PASSWORD": "postgres",
+    #     "HOST": os.getenv("DATABASE_URL", "db"),
+    #     "PORT": 5432,
+    # }
 }
 
 
